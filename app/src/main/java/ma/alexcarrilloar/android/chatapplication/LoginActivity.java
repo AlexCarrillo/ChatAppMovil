3409f13c3a37edd92deb3cd1307cdf072b9f9761
AlexCarrillo <alexcarrilloar@gmail.com>
2016-09-13 23:57:10 -0500

    It was added dependencies and packagingOptions to the gradle file It was create a FirebaseHelper class.

--- app/src/main/java/ma/alexcarrilloar/android/chatapplication/LoginActivity.java
+++ app/src/main/java/ma/alexcarrilloar/android/chatapplication/LoginActivity.java
@@ -1,13 +1,32 @@
 package ma.alexcarrilloar.android.chatapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.btnSignin)
+    Button btnSignin;
+    @Bind(R.id.btnSignup)
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignin)
+    public void onClickSignin() {
+    }
+
+    @OnClick(R.id.btnSignup)
+    public void onClickSignup() {
     }
 }
